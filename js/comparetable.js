d5be51e51015ee5efc5222d594cd5c6986bbfc27
Juanan Pereira <juanan@ikasten.io>
2019-10-30 08:32:44 +0100

    Initial working version of the project comparison table

--- js/comparetable.js
+++ js/comparetable.js
@@ -27,8 +27,12 @@ window.onload = function () {
             chrome.storage.sync.get(project, result => {
                 obj = {};
                 keys.forEach( key => {
-                    // tabledata[key].push(result[project][key].value);
-                    obj[key] = result[project][key].value;
+                    // FIXME: this is a dirty hack, avoid this conditional if
+                    if (key == "suitability")
+                        obj[key] = result[project][key];
+                    else
+                        obj[key] = result[project][key].value;
+                    // console.log(project + " " + key + " " + result[project][key].value);
                 })
                 obj["name"] = e.owner;
                 obj["id"] = id++;
@@ -39,11 +43,11 @@ window.onload = function () {
 
         setTimeout(() => {
 
-            console.log(rows);
+            // console.log(rows);
 
             tabla = new Tabulator("#example-table", {
                 data: rows,           //load row data from array
-                responsiveLayout: "hide",  //hide columns that dont fit on the table
+                // responsiveLayout: "hide",  //hide columns that dont fit on the table
                 tooltips: true,            //show tool tips on cells
                 addRowPos: "top",          //when adding a new row, add it to the top of the table
                 history: true,             //allow undo and redo actions on the table
@@ -54,15 +58,22 @@ window.onload = function () {
                 ],
                 columns: [                 //define the table columns
                     {title: "Project", field: "name"},
-                    // let keys = ["beginnerFriendly", "closingFactor", "continuousActivity", "contributionOpportunities", "docWeb", "docWiki", "forkDegree", "popularity", "pullRequests", "recentActivity", "suitability", "workForce"];
+                    {title: "suitability", field: "suitability", width: 130, editor: "input"},
                     {title: "beginnerFriendly" , field: "beginnerFriendly", width: 130, editor: "input"},
                     {title: "closingFactor", field: "closingFactor", width: 130, editor: "input"},
                     {title: "continuousActivity", field: "continuousActivity", width: 130, editor: "input"},
                     {title: "contributionOpportunities", field: "contributionOpportunities", width: 130, editor: "input"},
-                ],
+                    {title: "docWeb", field: "docWeb", width: 130, editor: "input"},
+                    {title: "docWiki", field: "docWiki", width: 130, editor: "input"},
+                    {title: "forkDegree", field: "forkDegree", width: 130, editor: "input"},
+                    {title: "popularity", field: "popularity", width: 130, editor: "input"},
+                    {title: "pullRequests", field: "pullRequests", width: 130, editor: "input"},
+                    {title: "recentActivity", field: "recentActivity", width: 130, editor: "input"},
+                    {title: "workForce", field: "workForce", width: 130, editor: "input"}
+        ],
             });
             // OSSinSE_Preferences.table = tabla;
-        }, 1000);
+        }, 250);
 
     });
 
